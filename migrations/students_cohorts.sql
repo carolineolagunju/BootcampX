aee2459e5b53e67e2f87682f271735a8508b9bb3
carolineolagunju <olamidejr21@gmail.com>
2023-07-26 23:46:09 -0600

    edit code

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -2,9 +2,10 @@ CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
-  end_date DATE);
+  end_date DATE
+);
 
- CREATE TABLE students (
+CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
@@ -13,4 +14,4 @@ CREATE TABLE cohorts (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-  );
+);
