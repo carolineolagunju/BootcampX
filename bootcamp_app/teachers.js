ad4d26a2d4622105a920f1eaad780781d1aac810
carolineolagunju <olamidejr21@gmail.com>
2023-08-03 23:18:06 -0600

    clean code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,10 +9,10 @@ const pool = new Pool({
 });
 
 //commandline args
-//const args = process.argv.slice(2);
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
 
+//query to be selected
 const query = `
 SELECT students.id, students.name, cohorts.name AS cohort_name
 FROM students
@@ -23,9 +23,9 @@ LIMIT $2;
 
 const values = [cohortName, limit];
 pool.query(query, values)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
-  });
-})
-.catch(err => console.error('query error', err.stack));
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
