3ffa886fc181f3bc378cc019efc9f916f47ea1fc
carolineolagunju <olamidejr21@gmail.com>
2023-08-03 23:13:25 -0600

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,19 +8,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//query to select all teachers that assisted
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
+//commandline args
+//const args = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const query = `
+SELECT students.id, students.name, cohorts.name AS cohort_name
+FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-  .then(res => {
-  //loop through the array of objects
-    res.rows.forEach(element => {
-      console.log(`${element.cohort}: ${element.teacher}`);
-    });
-  }).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name = $1
+LIMIT $2;
+`;
+
+const values = [cohortName, limit];
+pool.query(query, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  });
+})
+.catch(err => console.error('query error', err.stack));
